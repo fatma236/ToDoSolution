a65814a01b58cc31a70fe146caabc2b116c441f6
fatma236 <taim66207@gmail.com>
2025-12-15 09:47:45 +0400

    branch

--- TasksToGo/Models/AuthModel/AppUser.cs
+++ TasksToGo/Models/AuthModel/AppUser.cs
@@ -7,13 +7,14 @@ namespace TasksToGo.Models.AuthModel
     public class AppUser : IdentityUser
     {
        
-        public int UserId { get; set; }
+        
         [Required]
         [MaxLength(50)]
         public string fname { get; set; }
         [Required]
         [MaxLength(50)]
         public string lname { get; set; }
+        public string surName { get; set; }
         public string email { get; set; }
         public string phoneNumber { get; set; }
         public string password { get; set; }
---
+++ TasksToGo/ViewModel/RegisterVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TasksToGo.ViewModel
+{
+    public class RegisterVM
+    {
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string surName { get; set; }
+
+    }
+}
